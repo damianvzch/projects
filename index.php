118e0e229f1432fdc18a124c65329cd99b8b55eb
Miguel Ángel Damián <57847826+damianvzch@users.noreply.github.com>
2021-07-06 04:01:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+$listar = array();
+$directorio = opendir("../");
+while ($elemento = readdir($directorio)) {
+  if($elemento != '.' && $elemento != '..' && $elemento != 'projects' && $elemento != 'webalizer' && $elemento != 'img'){
+    if(is_dir("../".$elemento)){
+      array_push($listar,$elemento);
+      sort($listar);
+    }
+  }
+
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Proyectos</title>
+    <link href="img/server.png" rel="icon">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+    <div class="content" align="center">
+      <div class="menu">
+      <img src="img/server.png" width="150" alt="Proyectos">
+      <h4 align="center"><b>PROYECTOS</b></h4>
+      <hr>
+      <h4 align="left"><b>Base de Datos</b></h4>
+      <ol align="left">
+      <img src='img/database.gif' width='50'><a class='lista' href="../phpmyadmin" target='_blank'> phpMyAdmin</a>
+      </ol>
+        <h4 align="left"><b>htdocs</b></h4>
+     
+        <ol align="left">
+          <?php 
+          
+          foreach ($listar as $key => $value) {
+            echo "<img src='img/file.gif' width='50'><a class='lista' href='../$value' target='_blank'> $value</a></br>";
+          }
+    
+          
+          ?>
+        </ol>
+
+      </div>
+
+    </div>
+ 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
